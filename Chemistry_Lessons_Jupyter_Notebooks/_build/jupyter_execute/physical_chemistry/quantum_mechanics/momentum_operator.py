1b8f90a119f16f16c74a173780d6ea17c90bb1c6
McCullagh Group <martin.mccullagh@okstate.edu>
2023-02-09 15:08:32 -0600

    something

---
+++ Chemistry_Lessons_Jupyter_Notebooks/_build/jupyter_execute/physical_chemistry/quantum_mechanics/momentum_operator.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Momentum Operator
+
+# ## Motivation
+# 
+# We want to know why the (1D) momentum operator is $hat{p_x} = -i\hbar\frac{\partial}{\partial x}$.
+
+# ## Learning Goals
+# 
+# After working through this notebook, you will be able to 
+# 
+# 1. Use Fourier transforms to convert between position and momentum space
+# 2. Identify the Fourier integral form of the Dirac delta function
+# 3. Utilize Fourier transforms to derive the 1D momentum operator in position space.
+
+# ## Coding Concepts
+# 
+# The following coding concepts are used in this notebook
+# 
+# 1. [Variables](../../coding_concepts/variables.ipynb)
+# 2. [Functions](../../coding_concepts/functions.ipynb)
+# 3. [Plotting with matplotlib](../../coding_concepts/plotting_with_matplotlib.ipynb)
+
+# ## The Premise
+
+# We have been told that the two operators from which we can construct all others in quantum mechanics are the position operator and momentum operator.  In one dimension these are given as
+# \begin{align}
+# \hat{X} &= x \\
+# \hat{P_x} &= -i\hbar\frac{\partial}{\partial x}
+# \end{align}
+# 
+# It is the goal of these notes to demonstrate why the momentum operator in position coordinates is the first derivative.  
+# 
+# A single particle at finite temperature in one dimension will have a position, denoted $x$, and a momentum, denoted $p_x$.  The particle can be described by a wavefunction in position space, denoted $\psi(x)$, and a wave function in momentum space, denoted $\phi(p_x)$.  The position operator operating on the position wave function yields the position of the particle and the momentum operator in momentum space operating on the momentum wave function yields the moment.  This is more concisely written as 
+# \begin{align}
+# &\hat{X}|\psi(x)\rangle = x |\psi(x)\rangle \\
+# &\hat{P}|\phi(p_x)\rangle = p_x | \phi(p_x)\rangle
+# \end{align}
+# 
+# Our goal can be restated as determining the form of the operator $\hat{P}$ in position space.  
+
+# ### Fourier Transforms
+
+# In addition to setting up the problem, the crux of this derivation is recognizing that $\psi(x)$ and $\phi(p_x)$ are Fourier transforms of each other.  If you are not familiar with this type of transformation, do not worry.  This can be written mathematically as 
+# \begin{align}
+# \psi(x) &= \frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \phi(p_x) e^{ip_xx/\hbar}dp_x \\
+# \phi(p_x) &= \frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \psi(x) e^{-ip_xx/\hbar}dx 
+# \end{align}
+# 
+# Fourier transformations show up in a number of places but typically they are used to convert between a position space and a frequency space.  Here we are converting between position and momentum.
+# 
+# Another aspect that shows up in Fourier transforms that we will utilize is the Dirac delta function.  There are many definitions of this but the one that shows up in Fourier transforms is
+# \begin{equation}
+# \delta(x) = \frac{1}{2\pi}\int_{-\infty}^{\infty}e^{ixt}dt
+# \end{equation}
+# The Dirac delta function has the following property
+# \begin{equation}
+# \delta(x) = \begin{cases} 1\text{ if } x=0 \\ 0 \text{ otherwise}\end{cases}
+# \end{equation}
+# The integral of the Dirac delta function over all space is equal to 1
+# \begin{equation}
+# \int_{-\infty}^{\infty} \delta(x)dx = 1
+# \end{equation}
+# Additionally, the Dirac delta is often used in integral equations to remove dependency on one variable because of the following property
+# \begin{equation}
+# \int_{-\infty}^{\infty} \delta(x)f(x)dx = f(0),
+# \end{equation}
+# or, somewhat more generally
+# \begin{equation}
+# \int_{-\infty}^{\infty} \delta(x-x_0)f(x)dx = f(x_0).
+# \end{equation}
+# 
+# 
+# To see how this is true for the Fourier transform integral case, we will take the integral for finite limits and then argue that this function behaves as described for the Dirac delta function.
+# \begin{align}
+# \int_{-n}^{n}e^{ixt}dt &= \left[ \frac{e^{ixt}}{ix}\right]_{-n}^{n} \\
+# &= \frac{e^{ixn}-e^{-ixn}}{ix} \\
+# &= \frac{2i\sin xn}{ix} \\
+# &= \frac{2\sin xn}{x}
+# \end{align}
+# Now we take the limit
+# \begin{align}
+# \lim_{n\rightarrow\infty}\frac{2\sin xn}{x} = 2\pi\delta(x)
+# \end{align}
+
+# In[5]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+def f(x,n):
+    return 2*np.sin(x*n)/x
+# 
+x = np.arange(-10,10,0.001)
+# setup plot parameters
+fig = plt.figure(figsize=(6,6), dpi= 80, facecolor='w', edgecolor='k')
+ax = plt.subplot(111)
+ax.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
+ax.set_xlabel(r'$x/a$',size=20)
+ax.set_ylabel(r'$f(x)$',size=20)
+plt.tick_params(axis='both',labelsize=20)
+# plot quantum result
+ax.plot(x,f(x,1),label="n=1",lw=2)
+ax.plot(x,f(x,2),label="n=2",lw=2)
+ax.plot(x,f(x,3),label="n=3",lw=2)
+ax.plot(x,f(x,10),label="n=10",lw=2)
+plt.title("Dirac Delta Function",fontsize=20)
+# make legend
+ax.legend(fontsize=12,markerscale=5.0);
+
+
+#  ### Deriving the Momentum Operator
+
+# We now have all the pieces to derive the expression for the momentum operator in position space.  The map of how we will do this is we recognize
+# \begin{equation}
+# \langle p \rangle = \langle \phi(p_x)| p_x | \phi(p_x)\rangle
+# \end{equation}
+# 
+# We will transform the coordinates in the integrals on the r.h.s of the above equation using Fourier transforms.  Before we do that, however, it is useful to start by taking the spacial derivative of $\psi(x)$
+# \begin{align}
+# \frac{\partial}{\partial x}\psi(x) &= \frac{\partial}{\partial x}\frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \phi(p_x) e^{ip_xx/\hbar}dp_x \\
+# &= \frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \phi(p_x) \frac{\partial}{\partial x}e^{ip_xx/\hbar}dp_x \\
+# &= \frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \phi(p_x) \frac{ip_x}{\hbar}e^{ip_xx/\hbar}dp_x \\
+# &= \frac{i}{\hbar\sqrt{2\pi}}\int_{-\infty}^{\infty} \phi(p_x) p_xe^{ip_xx/\hbar}dp_x \\
+# \Rightarrow -i\hbar\frac{\partial}{\partial x}\psi(x) &= \frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} p_x\phi(p_x) e^{ip_xx/\hbar}dp_x
+# \end{align}
+# The above equation demonstrates that $i\hbar\frac{\partial}{\partial x}\psi(x)$ and $p_x\phi(p_x)$ are Fourier transforms of eachother.  This will be useful in our coordinate transformation.
+# 
+# Now, let's start
+# \begin{align}
+# \langle \phi(p_x)| p_x | \phi(p_x)\rangle = \int_{-\infty}^{\infty}\phi^*(p_x)p_x\phi(p_x)dp_x
+# \end{align}
+# In this equation we will substitute that
+# \begin{align}
+# \phi^*(p_x) &= \frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \psi^*(x') e^{ip_xx'/\hbar}dx' \\
+# p_x\phi(p_x) &= \frac{1}{\sqrt{2\pi}}\int_{\infty}^{\infty} \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)e^{-ip_xx/\hbar}dx 
+# \end{align}
+# where the first relationship is the complex conjugate of the Fourier transform of $\phi$ and the second relationship is the inverse Fourier transform that we just showed.  I use $x'$ in the first expression to disntinguish the integration variables in the two integrals.  Plugging these into the expectation value of $p$ equation yields
+# \begin{align}
+# \langle \phi(p_x)| p_x | \phi(p_x)\rangle &= \int_{-\infty}^{\infty}\phi^*(p_x)p_x\phi(p_x)dp_x \\
+# &= \int_{-\infty}^{\infty}\left[\frac{1}{\sqrt{2\pi}}\int_{-\infty}^{\infty} \psi^*(x') e^{ip_xx'/\hbar}dx'\right]\left[\frac{1}{\sqrt{2\pi}}\int_{\infty}^{\infty} \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)e^{-ip_xx/\hbar}dx\right]dp_x \\
+# &= \frac{1}{2\pi}\int_{-\infty}^{\infty}\left[\int_{-\infty}^{\infty} \psi^*(x') e^{ip_xx'/\hbar}dx'\right]\left[\int_{\infty}^{\infty} \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)e^{-ip_xx/\hbar}dx\right]dp_x \\
+# &=\frac{1}{2\pi}\int_{-\infty}^{\infty}\int_{-\infty}^{\infty}\int_{\infty}^{\infty} \psi^*(x') e^{ip_xx'/\hbar} \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)e^{-ip_xx/\hbar}dp_xdx'dx 
+# \end{align}
+# where in the last step I can put all terms inside a triple integral because the terms inside the $x'$ and $x$ integrals don't depend on the other integration variable.  I can now combine the two exponential terms (because $e^{ip_xx'/\hbar}$ is constant with respect to $\frac{\partial}{\partial x}$) and then integrate the result over $p_x$:
+# \begin{align}
+# \langle \phi(p_x)| p_x | \phi(p_x)\rangle &= \frac{1}{2\pi}\int_{-\infty}^{\infty}\int_{-\infty}^{\infty}\int_{\infty}^{\infty} \psi^*(x') e^{ip_xx'/\hbar} \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)e^{-ip_xx/\hbar}dp_xdx'dx \\
+# &= \frac{1}{2\pi}\int_{-\infty}^{\infty}\int_{-\infty}^{\infty}\int_{\infty}^{\infty} \psi^*(x') \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)e^{ip_x(x'-x)/\hbar}dp_xdx'dx \\
+# &= \frac{1}{2\pi}\int_{-\infty}^{\infty}\int_{-\infty}^{\infty} \psi^*(x') \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)\int_{\infty}^{\infty}e^{ip_x(x'-x)/\hbar}dp_xdx'dx \\
+# &= \frac{1}{2\pi}\int_{-\infty}^{\infty}\int_{-\infty}^{\infty} \psi^*(x') \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)2\pi\delta(x-x')dx'dx
+# \end{align}
+# where we recognize the Fourier version of the Dirac delta function in the last step.  The Dirac function now allows us to elimnate one of the integration variables (i.e. $\delta(x-x')$ is only nonzero when $x'=x$) yielding
+# \begin{align}
+# \langle \phi(p_x)| p_x | \phi(p_x)\rangle &= \frac{1}{2\pi}\int_{-\infty}^{\infty}\int_{-\infty}^{\infty} \psi^*(x') \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)2\pi\delta(x-x')dx'dx \\
+# &= \int_{-\infty}^{\infty}\int_{-\infty}^{\infty} \psi^*(x) \left(-i\hbar\frac{\partial}{\partial x}\psi(x)\right)dx \\
+# &= \langle \psi(x) | -i\hbar\frac{\partial}{\partial x} | \psi(x)\rangle
+# \end{align}
+# 
+# Thus
+# \begin{equation}
+# \hat{p_x} = -i\hbar\frac{\partial}{\partial x}
+# \end{equation}
+# 
---
+++ Chemistry_Lessons_Jupyter_Notebooks/_build/jupyter_execute/physical_chemistry/quantum_mechanics/particle_in_a_sphere.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Particle in a Sphere
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## Statement of the Problem
+
+# ## The Hamiltonian
+
+# ## Spherical Polar Coordinates
+
+# 
+# $\nabla^2_{r\theta\phi} = \frac{1}{r^2}\frac{\partial}{\partial r}\left(r^2\frac{\partial}{\partial r}\right) + \frac{1}{r^2\sin\theta}\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)+\frac{1}{r^2\sin^2\theta}\frac{\partial^2}{\partial^2\phi}$
+
+# ## Separtion of Variables in Spherical Coordinates
+
+# The Schrodinger equation in spherical polar coordinates is
+# 
+# $ -\frac{\hbar^2}{2m}\left[\frac{1}{r^2}\frac{\partial}{\partial r}\left(r^2\frac{\partial}{\partial r}\right) + \frac{1}{r^2\sin\theta}\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)+\frac{1}{r^2\sin^2\theta}\frac{\partial^2}{\partial^2\phi}\right] \psi(r,\theta,\phi) = E \psi(r,\theta,\phi)$.
+# 
+# Note that since $r$ does not change (bond doesn't vibrate), we can write $\psi(r_0,\theta,\phi) = BY(\theta,\phi)$ where $B$ is a constant.  We will thus try and solve for functions $Y(\theta,\phi)$. Also, all $\frac{\partial}{\partial r}$ terms go to zero. So now we have
+# 
+# $ -\frac{\hbar^2}{2\mu r_0^2}\left[\frac{1}{\sin\theta}\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)+\frac{1}{\sin^2\theta}\frac{\partial^2}{\partial^2\phi}\right] Y(\theta,\phi) = E Y(\theta,\phi)$.
+# 
+# Note that $\mu r_0^2 = I$.  We will solve this problem by the method of separation of variables. The Schrodinger equation can be algebraically rearranged to give us 
+# 
+# $ \left[\sin\theta\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)+\frac{2IE}{\hbar^2}\sin^2\theta \right] Y(\theta,\phi) = -\frac{\partial^2}{\partial^2\phi} Y(\theta,\phi)$.  
+# 
+# We now substitue $Y(\theta,\phi) = \Theta(\theta)\Phi(\phi)$ and $\beta = \frac{2IE}{\hbar^2}$ to get
+# 
+# $ \left[\sin\theta\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)+\beta\sin^2\theta \right] \Theta(\theta)\Phi(\phi) = -\frac{\partial^2}{\partial^2\phi} \Theta(\theta)\Phi(\phi)$.
+# 
+# Divide both sides of the equation above by $\Theta(\theta)\Phi(\phi)$ and note that the operator on the right-hand side is independent of $\theta$ to yield
+# 
+# $ \frac{\sin\theta}{\Theta(\theta)}\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)\Theta(\theta)+\beta\sin^2\theta = -\frac{1}{\Phi(\phi)}\frac{\partial^2}{\partial^2\phi} \Phi(\phi)$.
+# 
+# Now the left-hand side is independent of $\phi$ and the right-hand side is independent of $\theta$.  Since these two things are equal but independent of the other's variable they must be constant.  We will define this constant as $m^2$ (for reasons that will become clear later) and solve the following two equations independently
+# 
+# $m^2 = -\frac{1}{\Phi(\phi)}\frac{\partial^2}{\partial^2\phi} \Phi(\phi) \tag{1}$
+# 
+# $m^2 = \frac{\sin\theta}{\Theta(\theta)}\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)\Theta(\theta)+\beta\sin^2\theta \tag{2}$
+
+# ### Solutions to $\phi$ equation (1)
+
+# Equation (1) above can me simply rearranged to give
+# 
+# $-m^2 \Phi(\phi)= \frac{\partial^2}{\partial^2\phi} \Phi(\phi)$
+# 
+# which is straightforward eigenvalue-eigenvector problem with solutions
+# 
+# $\Phi(\phi) = A_me^{im\phi}\quad \mathrm{and}\quad A_{-m}e^{-im\phi}$.
+# 
+# Applying boundary conditions, $\Phi(\phi+2\pi) = \Phi(\phi)$ yields the quantization
+# 
+# $m=0,\pm 1, \pm 2, ...$
+# 
+# Thus we can write
+# 
+# $\Phi(\phi) = Ae^{im\phi} \quad m=0,\pm 1, \pm 2, ...$.
+# 
+# Normalization yields $A=\frac{1}{\sqrt{2\pi}}$.
+
+# ### Solutions to $\theta$ equation (2)
+
+# The solutions to equation (2) above are not as straightforward as those for equation (1).  We start by rewriting the original equation here:
+# 
+# $m^2 = \frac{\sin\theta}{\Theta(\theta)}\frac{\partial}{\partial\theta}\left(\sin\theta\frac{\partial}{\partial\theta}\right)\Theta(\theta)+\beta\sin^2\theta$.
+# 
+# Now make a change of variable $x = \cos\theta$ which yields $\frac{dx}{-\sin\theta}=d\theta$ and define $P(x) = \Theta(\theta)$.  Plugging these in an performing some rearrangements yields the Legendre equation
+# 
+# $(1-x^2)\frac{d^2}{dx^2}P(x)-2x\frac{d}{dx}P(x)+\left[\beta-\frac{m^2}{1-x^2}\right]P(x) = 0$.
+# 
+# For $\Theta(\theta)$ to be continous $\beta=J(J+1)$ where $J=0,1,2...$.  Note that this also puts a limit on $m$ with $m=0,\pm 1, \pm 2, ... , \pm J$.   The quantization of $\beta$ leads to the quantization of energy
+# 
+# $E_J = \frac{\hbar^2}{2I}J(J+1)$.
+# 
+# The solutions, $P(x)$, to the Legendre equation are known as the Associated Legendre polynomials.  
+# 
+# $P_\nu^m = (-1)^m(1-x^2)^{m/2}\frac{d^m}{dx^m}P_\nu(x)$
+# 
+# where
+# 
+# $P_\nu(x) = \sum_{k=0}^{\infty}\frac{(-\nu)_k(\nu+1)_k}{k!^2}\left(\frac{1-x}{2}\right)^k$
+# 
+# and $(\nu)_k = \frac{(\nu+k-1)!}{(\nu-1)!}$.
+
+# In[1]:
+
+
+# plot of some of the Legendre polynomials
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+from scipy.special import lpmv
+x = np.arange(-1,1,0.001)
+plt.figure(figsize=(12,6),dpi= 80, facecolor='w', edgecolor='k')
+plt.tick_params(axis='both',labelsize=20)
+plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
+for l in range(4):
+    for m in range(l):
+        label = "l=" + str(l) + ", m=" + str(m)
+        plt.plot(x,lpmv(m,l,x),lw=4,label=label)
+plt.legend(fontsize=16);
+
+
+# ### Combining Solutions to $\theta$ and $\phi$
+
+# The total wavefunctions are the product of $\Phi(\phi)$ and $\Theta(\theta)$.  It is easy to see
+# 
+# $Y_l^m(\theta,\phi)\propto P_l^{|m|}(\cos\theta)e^{im\phi}$.
+# 
+# These are the spherical harmonics. We will now look at some of these.
+
+# In[2]:
+
+
+# make two plots of the same spherical harmonic
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm, colors
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import sph_harm
+get_ipython().run_line_magic('matplotlib', 'inline')
+from scipy.special import sph_harm
+def plot_spherical_harmonic(m,l,theta=np.linspace(0,np.pi,100),phi=np.linspace(0,2*np.pi,100)):
+    THETA, PHI = np.meshgrid(theta, phi)
+    X = np.sin(THETA) * np.cos(PHI)
+    Y = np.sin(THETA) * np.sin(PHI)
+    Z = np.cos(THETA)
+    # Calculate the spherical harmonic Y(l,m) and normalize to [0,1]
+    fcolors = sph_harm(m, l, PHI, THETA).real
+    s = sph_harm(m, l, PHI, THETA).real
+    s /= s.max()
+    fmax, fmin = fcolors.max(), fcolors.min()
+    fcolors = (fcolors - fmin)/(fmax - fmin)
+    
+
+    # Set the aspect ratio to 1 so our sphere looks spherical
+    fig = plt.figure(figsize=(24,12),dpi= 80, facecolor='w', edgecolor='k')
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    ax.plot_surface(X, Y, Z,  rstride=1, cstride=1, facecolors=cm.seismic(fcolors))
+    ax.set_axis_off()
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    ax.plot_surface(X*s, Y*s, Z*s,  rstride=1, cstride=1, facecolors=cm.seismic(fcolors))
+    # Turn off the axis planes
+    ax.set_axis_off()
+    plt.show();
+plot_spherical_harmonic(0,1)
+plot_spherical_harmonic(0,2);
+
+
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+from scipy.special import spherical_jn
+x = np.arange(0.0, 10.0, 0.01)
+fig, ax = plt.subplots()
+ax.set_ylim(-0.5, 1.5)
+ax.set_title(r'Spherical Bessel functions $j_n$')
+for n in np.arange(0, 4):
+    ax.plot(x, spherical_jn(n, x), label=rf'$j_{n}$')
+plt.legend(loc='best')
+plt.show()
+
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+from scipy.special import spherical_yn
+x = np.arange(0.0, 10.0, 0.01)
+fig, ax = plt.subplots()
+ax.set_ylim(-1.5, 0.5)
+ax.set_title(r'Spherical Bessel functions $y_n$')
+for n in np.arange(0, 4):
+    ax.plot(x, spherical_yn(n, x), label=rf'$y_{n}$')
+plt.legend(loc='best')
+plt.show()
+
+
+# In[ ]:
+
+
+
+
